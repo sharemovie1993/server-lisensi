3faac6ac82ebbb4a8ab1120a4686861c20d06563
sharemovie1993 <sharemovie1993@gmail.com>
2026-06-22 11:25:20 +0700

    fix: change PM2 exec_mode to fork to prevent EADDRINUSE

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'licensing-server',
       script: 'server.js',
       instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '300M',
